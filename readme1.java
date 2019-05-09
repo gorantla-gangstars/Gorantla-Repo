c21631c2df77f19d16f736057b276046d76fc9ad
sravanikundurthi <sravanikundurthi3@gmail.com>
2019-05-09 08:32:09 +0530

    readme1 file commit

---
+++ readme1.java
@@ -0,0 +1,7 @@
+Hi Everyone
+
+Read the following command in git
+
+git push origin master - Push the changes in your local repository to github remote repository. 
+
+( Here push is the git command, origin is the remote name and master is the branch name "
