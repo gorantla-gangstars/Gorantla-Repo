ac93c6c0e607d54b83dae990ad3298b967a90f79
sravanikundurthi <sravanikundurthi3@gmail.com>
2019-05-09 08:49:08 +0530

    readme1 second commit

--- readme1.java
+++ readme1.java
@@ -5,3 +5,5 @@ Read the following command in git
 git push origin master - Push the changes in your local repository to github remote repository. 
 
 ( Here push is the git command, origin is the remote name and master is the branch name "
+
+git clean -f - Remove new files from working area
