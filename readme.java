7b02b345fef33208219c28f8d37189d615b4dea7
sravanikundurthi <sravanikundurthi3@gmail.com>
2019-05-09 08:14:46 +0530

    readme file first commit

---
+++ readme.java
@@ -0,0 +1,16 @@
+Hi Everyone
+This is a simple with the content of few git commands
+
+git init - Initialize local repository
+
+git branch - List branches
+
+git status - check status
+
+git log - It will give all commit id's
+
+git add . - Add one or more files to staging area
+
+........
+
+
